74ee38b26126ed0413e147ec20fbc9a1b5ef4768
Jaro Reinders <jaro.reinders@gmail.com>
2023-04-10 23:28:13 +0200

    experiment a bit

--- experiments/Main.hs
+++ experiments/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
-import Control.Applicative
-import Data.Char
+import Control.Applicative ( asum, Alternative((<|>), empty, many) )
+import Data.Char ( digitToInt, intToDigit )
 
 newtype Parser s a = Parser { parse :: String -> s -> [(a, String, s)] } deriving Functor
 
@@ -22,22 +22,30 @@ char c = Parser $ \xs s ->
     c' : xs' | c == c' -> [(c, xs', s)]
     _ -> []
 
-put :: Parser s' s -> Parser s a -> Parser s' a
+put :: Parser s' s -> Parser (s,s') a -> Parser s' a
 put (Parser p) (Parser q) = Parser $ \xs s -> do
-  (s', xs', _) <- p xs s
-  (x, xs'', _) <- q xs' s'
-  pure (x, xs'', s)
+  (s', xs', t') <- p xs s
+  (x, xs'', (_,t'')) <- q xs' (s',t')
+  pure (x, xs'', t'')
 
 modify :: (s -> [s]) -> Parser s ()
 modify f = Parser $ \xs s -> [((), xs, s') | s' <- f s]
 
-dependentReplicate :: Parser s Int -> Parser Int a -> Parser s [a]
-dependentReplicate p1 p2 = put p1 rest where
-  rest = (:) <$ modify (\s -> [s | s > 0]) <*> p2 <* modify (\s -> [s - 1]) <*> rest
-      <|> [] <$ modify (\s -> [s | s == 0])
+under :: Parser t a -> Parser (s, t) a
+under (Parser p) = Parser $ \xs (s0,t) -> do
+    (x, xs', t') <- p xs t
+    pure (x, xs', (s0, t'))
+
+dependentReplicate :: Parser s Int -> Parser s a -> Parser s [a]
+dependentReplicate p1 p2 = put p1 $
+  many (modify (\s -> [s | fst s > 0]) *> under p2 <* modify (\(s,t) -> [(s - 1, t)]))
+   <* modify (\s -> [s | fst s == 0])
 
 digit :: Parser s Int
 digit = asum [digitToInt <$> char (intToDigit i) | i <- [0..9]]
 
 main :: IO ()
-main = print $ parse (dependentReplicate digit (char '.')) "4...." ()
+main = do
+    print $ parse (dependentReplicate digit (char '.')) "4..." ()
+    print $ parse (dependentReplicate digit (char '.')) "4...." ()
+    print $ parse (dependentReplicate digit (char '.')) "4....." ()
