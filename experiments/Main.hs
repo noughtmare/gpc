bf823757192126522c9303ea3dfe972ea11c00d9
Jaro Reinders <jaro.reinders@gmail.com>
2023-04-14 17:45:31 +0200

    experiment

--- experiments/Main.hs
+++ experiments/Main.hs
@@ -1,51 +1,70 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE RankNTypes #-}
 import Control.Applicative ( asum, Alternative((<|>), empty, many) )
 import Data.Char ( digitToInt, intToDigit )
+import Data.Bifunctor
+import Data.Functor.Const
+import qualified Data.IntMap as M
+import GHC.Exts (Any)
 
-newtype Parser s a = Parser { parse :: String -> s -> [(a, String, s)] } deriving Functor
+data Env = Env !Int (M.IntMap Any)
+newtype Ref a = Ref Int
 
-instance Applicative (Parser s) where
+emptyEnv :: Env
+emptyEnv = undefined
+
+lookupEnv :: Env -> Ref a -> a
+lookupEnv = undefined
+
+modifyEnv :: Env -> Ref a -> (a -> a) -> Env
+modifyEnv = undefined
+
+freshEnv :: Env -> a -> (Ref a, Env)
+freshEnv = undefined
+
+newtype Parser a = Parser { parse :: String -> Env -> [(a, String, Env)] } deriving Functor
+
+instance Applicative Parser where
   pure x = Parser $ \xs s -> [(x, xs, s)]
   Parser p <*> Parser q = Parser $ \xs s -> do
     (f, xs', s') <- p xs s
     (x, xs'', s'') <- q xs' s'
     pure (f x, xs'', s'')
 
-instance Alternative (Parser s) where
+instance Alternative Parser where
   empty = Parser $ \_ _ -> []
   Parser p <|> Parser q = Parser $ \xs s ->
     p xs s ++ q xs s
 
-char :: Char -> Parser s Char
+char :: Char -> Parser Char
 char c = Parser $ \xs s ->
   case xs of
     c' : xs' | c == c' -> [(c, xs', s)]
     _ -> []
 
-put :: Parser s' s -> Parser (s,s') a -> Parser s' a
-put (Parser p) (Parser q) = Parser $ \xs s -> do
-  (s', xs', t') <- p xs s
-  (x, xs'', (_,t'')) <- q xs' (s',t')
-  pure (x, xs'', t'')
+with :: Parser a -> (Ref a -> Parser b) -> Parser b
+with (Parser p) f = Parser $ \s env -> do
+    (x, s', env') <- p s env
+    let (r, env'') = freshEnv env' x
+    parse (f r) s' env''
 
-modify :: (s -> [s]) -> Parser s ()
-modify f = Parser $ \xs s -> [((), xs, s') | s' <- f s]
+modify :: Ref s -> (s -> s) -> Parser ()
+modify = undefined
 
-under :: Parser t a -> Parser (s, t) a
-under (Parser p) = Parser $ \xs (s0,t) -> do
-    (x, xs', t') <- p xs t
-    pure (x, xs', (s0, t'))
+guard :: Ref s -> (s -> Bool) -> Parser ()
+guard = undefined
 
-dependentReplicate :: Parser s Int -> Parser s a -> Parser s [a]
-dependentReplicate p1 p2 = put p1 $
-  many (modify (\s -> [s | fst s > 0]) *> under p2 <* modify (\(s,t) -> [(s - 1, t)]))
-   <* modify (\s -> [s | fst s == 0])
+dependentReplicate :: Parser Int -> Parser a -> Parser [a]
+dependentReplicate p1 p2 = with p1 $ \s ->
+  many (guard s (> 0) *> p2 <* modify s (- 1))
+   <* guard s (== 0)
 
-digit :: Parser s Int
+digit :: Parser Int
 digit = asum [digitToInt <$> char (intToDigit i) | i <- [0..9]]
 
 main :: IO ()
 main = do
-    print $ parse (dependentReplicate digit (char '.')) "4..." ()
-    print $ parse (dependentReplicate digit (char '.')) "4...." ()
-    print $ parse (dependentReplicate digit (char '.')) "4....." ()
+    print $ map (\(x,y,_) -> (x,y)) $ parse (dependentReplicate digit (char '.')) "4..." emptyEnv
+    print $ map (\(x,y,_) -> (x,y)) $ parse (dependentReplicate digit (char '.')) "4...." emptyEnv
+    print $ map (\(x,y,_) -> (x,y)) $ parse (dependentReplicate digit (char '.')) "4....." emptyEnv
