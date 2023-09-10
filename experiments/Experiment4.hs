802018531599a7254318197e5aa4c019c8d7452e
Jaro Reinders <jaro.reinders@gmail.com>
2023-09-10 23:04:23 +0200

    Add semantic actions

--- experiments/Experiment4.hs
+++ experiments/Experiment4.hs
@@ -12,27 +12,37 @@ import Data.Map.Strict qualified as Map
 import Test.Tasty.Bench.Fit
 import Data.Text qualified as Text
 import Data.Text (Text)
+import Data.Some
+import Data.Functor.Identity
+import Data.GADT.Show
+import Data.Bifunctor
+import Unsafe.Coerce (unsafeCoerce)
+import Data.GADT.Compare
+import Data.Type.Equality
 
-data RHS f a = Pure a | T Char (RHS f a) | NT f (RHS f a) | Or (RHS f a) (RHS f a) | Fail
-  deriving (Show, Functor)
+data RHS f a = Pure a | T Char (RHS f a) | forall x. NT (f x) (x -> RHS f a) | Or (RHS f a) (RHS f a) | Fail
+deriving instance Functor (RHS f)
 
 instance Applicative (RHS f) where
   pure = Pure
-  Pure f <*> k = fmap f k
-  T c k <*> k' = T c (k <*> k')
-  NT f k <*> k' = NT f (k <*> k')
-  Or p q <*> k = Or (p <*> k) (q <*> k)
-  Fail <*> _ = Fail
+  (<*>) = ap
 
 instance Alternative (RHS f) where
   (<|>) = Or
   empty = Fail
 
-data CFG f = CFG f (f -> RHS f ())
+instance Monad (RHS f) where
+  Pure x >>= k = k x
+  T c k >>= k' = T c (k >>= k')
+  NT f k >>= k' = NT f (k >=> k')
+  Or p q >>= k = Or (p >>= k) (q >>= k)
+  Fail >>= _ = Fail
+
+data CFG f a = CFG (f a) (forall x. f x -> RHS f x)
 
 data T3 a b c = T3 !a !b !c deriving Show
 
-data Comm f = Comm !f !Int deriving (Eq, Ord, Show)
+data Comm f = Comm !(Some f) !Int deriving (Eq, Ord, Show)
 
 newtype Cont f a = Cont { getCont :: Text -> Descr -> a -> Command f }
 instance Show (Cont f a) where
@@ -56,55 +66,66 @@ initRel x (Rel m) = Rel (Map.insertWith (++) x [] m)
 addRel :: Ord a => a -> b -> Rel a b -> Rel a b
 addRel x y (Rel m) = Rel (Map.insertWith (++) x [y] m)
 
--- newtype U = U (Set Descr)
-newtype G f = G { getG :: Rel (Comm f) (Slot, Int, Cont f ()) } deriving Show
-newtype P f = P { getP :: Rel (Comm f) Int } deriving Show
+instance GShow Identity where
+  gshowsPrec _ _ = showString "<Identity>"
+instance GShow (Cont f) where
+  gshowsPrec _ _ = showString "<Cont>"
 
-newtype Command f = Command { getCommand :: T3 (G f) (P f) Bool -> T3 (G f) (P f) Bool }
+-- newtype U = U (Set Descr)
+newtype G f = G { getG :: Rel (Comm f) (Slot, Int, Some (Cont f)) } deriving Show
+newtype P f = P { getP :: Rel (Comm f) (Int, Some Identity) } deriving Show
+
+newtype Command f = Command { getCommand :: forall x. T3 (G f) (P f) (Maybe x) -> T3 (G f) (P f) (Maybe x) }
 
 newtype M f a = M { getM :: Text -> Descr -> Cont f a -> Command f }
 
-extents :: Ord f => f -> M f (Maybe [Int])
-extents nt = M (\inp dsc@(Descr _ _ i) k ->
+extents :: GCompare f => f a -> M f (Maybe [(Int, a)])
+extents nt = M $ \inp dsc@(Descr _ _ i) (Cont k) ->
   Command $ \(T3 g p b) -> -- trace ("extents " ++ show (nt, i)) $
-  getCommand (getCont k inp dsc (relMay (getP p) (Comm nt i))) (T3 g (P (initRel (Comm nt i) (getP p))) b))
+  getCommand
+    (k inp dsc (
+      let res = relMay (getP p) (Comm (Some nt) i)
+      in fmap (map (second (\(Some (Identity x)) -> unsafeCoerce x))) res))
+    (T3 g (P (initRel (Comm (Some nt) i) (getP p))) b)
 
-addExtent :: Ord f => f -> M f ()
-addExtent nt = M $ \inp dsc@(Descr _ l i) k ->
+addExtent :: GCompare f => f a -> a -> M f ()
+addExtent nt x = M $ \inp dsc@(Descr _ l i) (Cont k) ->
   Command $ \(T3 g p b) -> -- trace ("addExtent " ++ show (nt, l, i)) $
-  getCommand (getCont k inp dsc ()) (T3 g (P (addRel (Comm nt l) i (getP p))) b)
+  getCommand (k inp dsc ()) (T3 g (P (addRel (Comm (Some nt) l) (i, Some (Identity x)) (getP p))) b)
 
-resume :: Ord f => f -> M f ()
-resume nt = M $ \inp (Descr Slot l r) _ ->
+resume :: GCompare f => f a -> a -> M f a
+resume nt x = M $ \inp (Descr Slot l r) _ ->
   Command $ \(T3 g p b) ->
-    let cnts = rel (getG g) (Comm nt l) in -- trace ("resume " ++ show (nt, l, cnts)) $
-    foldr (\(s, l', Cont k) go -> go . getCommand (k inp (Descr s l' r) ()))
+    let cnts = rel (getG g) (Comm (Some nt) l) in -- trace ("resume " ++ show (nt, l, cnts)) $
+    foldr (\(s, l', Some (Cont k)) go -> go . getCommand (unsafeCoerce k inp (Descr s l' r) x))
       id cnts (T3 g p b)
 
-addCont :: Ord f => f -> M f () -> M f ()
+addCont :: GCompare f => f a -> M f c -> M f c
 addCont nt m = M $ \inp dsc@(Descr s l i) k ->
   Command $ \(T3 g p b) -> -- trace ("addCont " ++ show (nt, i)) $
-    getCommand (getM m inp dsc k) (T3 (G (addRel (Comm nt i) (s, l, k) (getG g))) p b)
+    getCommand (getM m inp dsc k) (T3 (G (addRel (Comm (Some nt) i) (s, l, Some k) (getG g))) p b)
 
 match :: Char -> M f ()
-match c = M $ \inp (Descr (Slot {- nt alpha beta -}) l i) k ->
+match c = M $ \inp (Descr (Slot {- nt alpha beta -}) l i) (Cont k) ->
   case Text.uncons inp of
-    Just (x,inp') | c == x -> getCont k inp' (Descr (Slot {- nt alpha beta -}) l (i + 1)) ()
+    Just (x,inp') | c == x -> k inp' (Descr (Slot {- nt alpha beta -}) l (i + 1)) ()
     _ -> Command id
 
 skip :: Int -> M f ()
-skip r = M $ \inp (Descr s l i) k -> getCont k (Text.drop (r - i) inp) (Descr s l r) ()
+skip r = M $ \inp (Descr s l i) (Cont k) -> k (Text.drop (r - i) inp) (Descr s l r) ()
 
 descend :: M f ()
-descend = M $ \inp (Descr Slot _ i) k -> getCont k inp (Descr Slot i i) ()
+descend = M $ \inp (Descr Slot _ i) (Cont k) -> k inp (Descr Slot i i) ()
 
 -- traceI :: String -> M ()
 -- traceI msg = M $ \inp dsc@(Descr _ _ i) k -> trace (show i ++ ": " ++ msg) getCont k inp dsc ()
 
 instance Functor (M f) where
-  fmap f (M p) = M $ \inp dsc k -> p inp dsc (Cont (\inp' dsc' x -> getCont k inp' dsc' (f x)))
+  fmap f (M p) = M $ \inp dsc (Cont k) ->
+    p inp dsc $ Cont $ \inp' dsc' x ->
+      k inp' dsc' (f x)
 instance Applicative (M f) where
-  pure x = M $ \inp dsc k -> getCont k inp dsc x
+  pure x = M $ \inp dsc (Cont k) -> k inp dsc x
   (<*>) = ap
 instance Alternative (M f) where
   empty = M $ \_ _ _ -> Command id
@@ -114,31 +135,31 @@ instance Monad (M f) where
     p inp dsc $ Cont $ \inp' dsc' x ->
       getM (k x) inp' dsc' k'
 
-parseCFG :: forall f. Ord f => CFG f -> Text -> T3 (G f) (P f) Bool
+parseCFG :: forall f a. GCompare f => CFG f a -> Text -> T3 (G f) (P f) (Maybe a)
 parseCFG (CFG nt0 prods) inp0 =
   getCommand
-    (getM (parseRHS (NT nt0 (pure ()))) inp0 (Descr Slot 0 0) final)
-    (T3 (G (Rel mempty)) (P (Rel mempty)) False) where
+    (getM (parseRHS (NT nt0 pure)) inp0 (Descr Slot 0 0) final)
+    (T3 (G (Rel mempty)) (P (Rel mempty)) Nothing) where
 
-  final :: Cont f ()
-  final = Cont $ \inp _ _ -> Command $ \(T3 p g b) -> (T3 p g (b || Text.null inp))
+  final :: Cont f a
+  final = Cont $ \inp _ x -> Command $ \(T3 p g b) -> (T3 p g (b <|> unsafeCoerce x <$ guard (Text.null inp)))
 
-  parseRHS :: RHS f () -> M f ()
-  parseRHS (Pure ()) = pure ()
+  parseRHS :: RHS f x -> M f x
+  parseRHS (Pure x) = pure x
   parseRHS (T c k) = parseT c *> parseRHS k
-  parseRHS (NT f k) = parseNT f *> parseRHS k
+  parseRHS (NT f k) = parseNT f >>= parseRHS . k
   parseRHS (Or p q) = parseRHS p <|> parseRHS q
   parseRHS Fail = empty
 
-  parseNT :: f -> M f ()
+  parseNT :: f x -> M f x
   parseNT nt = addCont nt $
     extents nt >>= \case
       Nothing -> do
         descend
-        parseRHS (prods nt)
-        addExtent nt
-        resume nt
-      Just rs -> asum (map skip rs)
+        x <- parseRHS (prods nt)
+        addExtent nt x
+        resume nt x
+      Just rs -> asum (map (\(r, x) -> x <$ skip r) rs)
 
   parseT :: Char -> M f ()
   parseT = match
@@ -146,34 +167,51 @@ parseCFG (CFG nt0 prods) inp0 =
 t :: Char -> RHS f ()
 t c = T c (pure ())
 
-nt :: f -> RHS f ()
-nt f = NT f (pure ())
+nt :: f a -> RHS f a
+nt f = NT f pure
 
-data E = E deriving (Eq, Ord, Show)
+data E a where E :: E Int
 
-example :: CFG E
-example = CFG E $ \E -> nt E *> t '+' *> nt E <|> t 'a'
+deriving instance Eq (E a)
+deriving instance Ord (E a)
+instance GEq E where
+  geq E E = Just Refl
+instance GCompare E where
+  gcompare E E = GEQ
+deriving instance Show (E a)
+
+example :: CFG E Int
+example = CFG E $ \E -> nt E *> t '+' *> nt E <|> 0 <$ t 'a'
 
 -- >>> parseCFG example "a+a+a+a+a+a"
 -- (G {getG = Rel (fromList [(Comm "E" 0,[(Slot,0,<Cont>),(Slot,0,<Cont>)]),(Comm "E" 2,[(Slot,2,<Cont>),(Slot,0,<Cont>)]),(Comm "E" 4,[(Slot,2,<Cont>),(Slot,4,<Cont>),(Slot,0,<Cont>)]),(Comm "E" 6,[(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,4,<Cont>),(Slot,6,<Cont>),(Slot,0,<Cont>)]),(Comm "E" 8,[(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,4,<Cont>),(Slot,6,<Cont>),(Slot,8,<Cont>),(Slot,0,<Cont>)]),(Comm "E" 10,[(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,0,<Cont>),(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,0,<Cont>),(Slot,2,<Cont>),(Slot,0,<Cont>),(Slot,4,<Cont>),(Slot,0,<Cont>),(Slot,0,<Cont>),(Slot,4,<Cont>),(Slot,0,<Cont>),(Slot,6,<Cont>),(Slot,8,<Cont>),(Slot,10,<Cont>),(Slot,0,<Cont>)])])},P {getP = Rel (fromList [(Comm "E" 0,[11,11,11,11,9,11,11,11,11,9,11,11,11,9,7,11,11,9,11,11,11,11,9,11,11,9,7,5,11,11,11,9,11,11,11,9,7,11,11,9,11,11,11,11,9,11,11,11,9,7,11,11,9,11,11,11,9,11,11,11,9,7,5,3,1]),(Comm "E" 2,[11,11,11,11,9,11,11,11,9,11,11,9,7,11,11,11,9,11,11,9,7,5,3]),(Comm "E" 4,[11,11,11,9,11,11,9,7,5]),(Comm "E" 6,[11,11,9,7]),(Comm "E" 8,[11,9]),(Comm "E" 10,[11])])},True)
 
-data N = N deriving (Eq, Ord, Show)
+data N a where N :: N Int
+deriving instance Eq (N a)
+deriving instance Ord (N a)
+deriving instance Show (N a)
+instance GEq N where
+  geq N N = Just Refl
+instance GCompare N where
+  gcompare N N = GEQ
+instance GShow N where
+  gshowsPrec _ N = showString "N"
 
-example3 :: CFG N
-example3 = CFG N $ \N -> t 'a' *> nt N <|> pure ()
+example3 :: CFG N Int
+example3 = CFG N $ \N -> (+ 1) <$ t 'a' <*> nt N <|> pure 0
 
-example4 :: CFG N
-example4 = CFG N $ \N -> nt N *> t 'a' <|> pure ()
+example4 :: CFG N Int
+example4 = CFG N $ \N -> (+ 1) <$> nt N <* t 'a' <|> pure 0
 
 -- >>> parseCFG example3 (Text.pack "aaaa")
--- T3 (G {getG = Rel (fromList [(Comm N 0,[(Slot,0,<Cont>)]),(Comm N 1,[(Slot,0,<Cont>)]),(Comm N 2,[(Slot,1,<Cont>)]),(Comm N 3,[(Slot,2,<Cont>)]),(Comm N 4,[(Slot,3,<Cont>)])])})
---    (P {getP = Rel (fromList [(Comm N 0,[0,1,2,3,4]),(Comm N 1,[1,2,3,4]),(Comm N 2,[2,3,4]),(Comm N 3,[3,4]),(Comm N 4,[4])])})
---    True
+-- T3 (G {getG = Rel (fromList [(Comm (Some N) 0,[(Slot,0,Some <Cont>)]),(Comm (Some N) 1,[(Slot,0,Some <Cont>)]),(Comm (Some N) 2,[(Slot,1,Some <Cont>)]),(Comm (Some N) 3,[(Slot,2,Some <Cont>)]),(Comm (Some N) 4,[(Slot,3,Some <Cont>)])])})
+--    (P {getP = Rel (fromList [(Comm (Some N) 0,[(0,Some <Identity>),(1,Some <Identity>),(2,Some <Identity>),(3,Some <Identity>),(4,Some <Identity>)]),(Comm (Some N) 1,[(1,Some <Identity>),(2,Some <Identity>),(3,Some <Identity>),(4,Some <Identity>)]),(Comm (Some N) 2,[(2,Some <Identity>),(3,Some <Identity>),(4,Some <Identity>)]),(Comm (Some N) 3,[(3,Some <Identity>),(4,Some <Identity>)]),(Comm (Some N) 4,[(4,Some <Identity>)])])})
+--    (Just 4)
 
 -- >>> parseCFG example4 (Text.pack "aaaa")
--- T3 (G {getG = Rel (fromList [(Comm N 0,[(Slot,0,<Cont>),(Slot,0,<Cont>)])])})
---    (P {getP = Rel (fromList [(Comm N 0,[4,3,2,1,0])])})
---    True
+-- T3 (G {getG = Rel (fromList [(Comm (Some N) 0,[(Slot,0,Some <Cont>),(Slot,0,Some <Cont>)])])})
+--    (P {getP = Rel (fromList [(Comm (Some N) 0,[(4,Some <Identity>),(3,Some <Identity>),(2,Some <Identity>),(1,Some <Identity>),(0,Some <Identity>)])])})
+--    (Just 4)
 
 main :: IO ()
 -- main = print (parseCFG example "a+a+a")
